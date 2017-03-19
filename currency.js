a127adf121242be2b942c1d9da680f78f1eb57a8
Jay Harris <jay.harris@outlook.co.nz>
2017-03-20 12:07:22 +1300

    Adds a mapping of currency to currency name

--- currency.js
+++ currency.js
@@ -1,5 +1,40 @@
 const request = require('request');
 
+const currencyNames = {
+    'EUR': 'Euro',
+    'AUD': 'Australian Dollar',
+    'BGN': 'Bulgarian Lev',
+    'BRL': 'Brazilian Real',
+    'CAD': 'Canadian Dollar',
+    'CHF': 'Swiss Franc',
+    'CNY': 'Chinese Yuan',
+    'CZK': 'Czech Republic Koruna',
+    'DKK': 'Danish Krone',
+    'GBP': 'British Pound',
+    'HKD': 'Hong Kong Dollar',
+    'HRK': 'Croatian Kuna',
+    'HUF': 'Hungarian Forint',
+    'IDR': 'Indonesian Rupiah',
+    'ILS': 'Israeli New Shekel ',
+    'INR': 'Indian Rupee',
+    'JPY': 'Japanese Yen',
+    'KRW': 'South Korean Won',
+    'MXN': 'Mexican Peso',
+    'MYR': 'Malaysian Ringgit',
+    'NOK': 'Norwegian Krone',
+    'NZD': 'New Zealand Dollar',
+    'PHP': 'Philippine Peso',
+    'PLN': 'Polish Zloty',
+    'RON': 'Romanian Leu',
+    'RUB': 'Russian Ruble',
+    'SEK': 'Swedish Krona',
+    'SGD': 'Singapore Dollar',
+    'THB': 'Thai Baht',
+    'TRY': 'Turkish Lira',
+    'USD': 'United States Dollar',
+    'ZAR': 'South African Rand'
+};
+
 const supportedCurrencies = [
     'EUR',
     'AUD',
